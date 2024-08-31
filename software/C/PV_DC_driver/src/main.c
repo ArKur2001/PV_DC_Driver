41afc29cbc11e35886e0520f463dec03fd90f9b5
ArKur2001 <arekkurnik006@wp.pl>
2024-08-31 16:19:14 +0200

    led_blinking_test_added

--- software/C/PV_DC_driver/src/main.c
+++ software/C/PV_DC_driver/src/main.c
@@ -1,1 +1,24 @@
-void app_main() {}
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "driver/gpio.h"
+
+void app_main() 
+{
+
+    gpio_set_direction(2, GPIO_MODE_OUTPUT);
+
+    while(1)
+    {
+        gpio_set_level(2, 1);
+        printf("led_on\n");
+        vTaskDelay(pdMS_TO_TICKS(500));
+        
+        gpio_set_level(2, 0);
+        printf("led_off\n");
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
